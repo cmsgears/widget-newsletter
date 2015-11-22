4369ff7c6f5ed5fa2e533fd9d7e0720c848bd958
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-11-22 19:03:53 +0530

    Renamed frm-icon-field to frm-icon-element.

--- views/simple.php
+++ views/simple.php
@@ -2,7 +2,7 @@
 		<div class="max-area-cover spinner"><div class="valign-center cmti cmti-2x cmti-spinner-1 spin"></div></div>
 
 		<div class="frm-field">
-			<div class="frm-icon-field email">
+			<div class="frm-icon-element email">
 				<i class="cmti cmti-at"></i><input type="text" name="Newsletter[email]" placeholder="Email *">
 			</div>
 			<span class="error" cmt-error="email"></span>
