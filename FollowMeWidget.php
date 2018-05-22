a282b1ee7059447198a92f3980d428e86b8b98e9
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-05-22 18:03:22 +0530

    Applied quick fixes.

---
+++ FollowMeWidget.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
+namespace cmsgears\widgets\newsletter;
+
+// Yii Imports
+use yii\helpers\Html;
+
+// CMG Imports
+use cmsgears\core\common\base\Widget;
+
+class FollowMeWidget extends Widget {
+
+	// Variables ---------------------------------------------------
+
+	// Globals -------------------------------
+
+	// Constants --------------
+
+	// Public -----------------
+
+	// Protected --------------
+
+	// Variables -----------------------------
+
+	// Public -----------------
+
+    public $btnText	= "Join Us";
+
+	public $ajaxUrl	= 'newsletter/site/sign-up'; // CMT App Request - Submit Path
+
+	// CMT JS Framework to handle ajax request
+	public $cmtApp			= 'site';
+    public $cmtController	= 'default';
+    public $cmtAction		= 'default';
+
+	// Protected --------------
+
+	// Private ----------------
+
+	// Traits ------------------------------------------------------
+
+	// Constructor and Initialisation ------------------------------
+
+	// Instance methods --------------------------------------------
+
+	// Yii interfaces ------------------------
+
+	// Yii parent classes --------------------
+
+	// yii\base\Widget
+
+	// CMG interfaces ------------------------
+
+	// CMG parent classes --------------------
+
+	// FollowMeWidget ------------------------
+
+	public function renderWidget( $config = [] ) {
+
+		$widgetHtml = $this->render( $this->template, [ 'widget' => $this ] );
+
+		if( $this->wrap ) {
+
+			return Html::tag( $this->wrapper, $widgetHtml, $this->options );
+		}
+
+		return $widgetHtml;
+	}
+
+}
---
+++ views/default.php
@@ -0,0 +1,31 @@
+<?php
+$btnText = $widget->btnText;
+
+$ajaxUrl		= $widget->ajaxUrl;
+$cmtApp			= $widget->cmtApp;
+$cmtController	= $widget->cmtController;
+$cmtAction		= $widget->cmtAction;
+?>
+<form class="form row max-cols-100" cmt-app="<?= $cmtApp ?>" cmt-controller="<?= $cmtController ?>" cmt-action="<?= $cmtAction ?>" action="<?= $ajaxUrl ?>">
+	<div class="max-area-cover spinner">
+		<div class="valign-center cmti cmti-2x cmti-spinner-1 spin"></div>
+	</div>
+
+    <div class="frm-field">
+		<div class="frm-icon-element">
+			<i class="cmti cmti-at"></i>
+			<input type="text" name="Newsletter[email]" placeholder="Email *">
+			<span class="error" cmt-error="email"></span>
+		</div>
+    </div>
+
+	<div class="frm-actions">
+		<input class="submit" type="submit" name="submit" value="<?= $btnText ?>">
+	</div>
+
+    <div class="filler-height"></div>
+	<div class="message success"></div>
+	<div class="message warning"></div>
+	<div class="message error"></div>
+	<div class="filler-height"> </div>
+</form>
